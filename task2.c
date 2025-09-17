c495c72accbe328b7ddcfe282a6919521a1f641d
Ahsan Abbas <K253085@nu.edu.pk>
2025-09-17 08:52:07 -0700

    Add files via upload

--- task2.c
+++ task2.c
@@ -1,25 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
-{
-	int pizzas, total_cost=0 ;
-	printf("How many Pizzas do you want(1, 2, or 3):");
-	scanf("%d", &pizzas);
-	if(pizzas==1)
-	{
-		total_cost = 8 ;
-	}
-	else if(pizzas==2)
+ {
+    int pizzas;
+    printf("How many small pizzas do you want? ");
+    scanf("%d", &pizzas);
+
+    if (pizzas > 1)
     {
-	total_cost = 15 ;
-	}
-	else if(pizzas==3)
-    {
-	total_cost = 21 ;
-    }  
+        printf("Check our multi-pizza deals.\n");
+    }
 	else
 	{
-	printf("\nInvalid Choice.");   
+	 printf("Your total is $8.\n");
     }
-    printf("\nTotal Cost of %d Pizzas is %d $ .", pizzas ,total_cost);
-    return 0 ;
+    return 0;
 }
